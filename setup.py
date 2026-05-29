7585425c154c36f2021761543304fc24bf2b3b2f
Focht <fabian.chipana@unmsm.edu.pe>
2026-05-29 17:03:10 -0500

    fix: change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="data_tools",
-  version="4.1.1",
+  version="4.1.2",
   packages=find_packages(where="src"),
   package_dir={"": "src"},
   install_requires=[
